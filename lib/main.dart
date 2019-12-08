6099a418e21c7fdcce48eef5e80bc1475e42d676
Eria <c121680118@gmail.com>
2019-12-09 01:54:10 +0800

    add function

--- lib/main.dart
+++ lib/main.dart
@@ -19,15 +19,35 @@ void main() {
 class DicePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Row(
-      children: <Widget>[
-        Expanded(
-          child: Image.asset('images/dice1.png'),
-        ),
-        Expanded(
-          child: Image.asset('images/dice1.png'),
-        )
-      ],
+    return Center(
+      child: Row(
+        children: <Widget>[
+          Expanded(
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: FlatButton(
+                onPressed: btnClickEvent,
+                child: Image.asset('images/dice1.png'),
+              ),
+            ),
+          ),
+          Expanded(
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: FlatButton(
+                onPressed: () {
+                  print('object');
+                },
+                child: Image.asset('images/dice1.png'),
+              ),
+            ),
+          ),
+        ],
+      ),
     );
   }
+}
+
+void btnClickEvent() {
+  print('btnClickEvent...');
 }
